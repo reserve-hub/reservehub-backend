06d184d9f76716225712b55fca8845bf2798f7eb
JuanMauwu <juanmavc111@gmail.com>
2026-03-30 18:38:00 -0500

    Initial commit

---
+++ src/main/java/com/eap15/reservehub/ReserveHubApplication.java
@@ -0,0 +1,13 @@
+package com.eap15.reservehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReserveHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReserveHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eap15/reservehub/ReservehubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eap15.reservehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
